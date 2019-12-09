d7dcfdc5febf8f05180b0f2a2762a6e9703eb196
Tom Holmes <tom@wandb.com>
2019-12-08 18:02:52 -0800

    0020

---
+++ 0020-valid-parentheses/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isValid("()"))
+	fmt.Println(isValid("()[]{}"))
+	fmt.Println(isValid("(]"))
+	fmt.Println(isValid("([)]"))
+	fmt.Println(isValid("{[]}"))
+}
+
+func isValid(s string) bool {
+	var stack []byte
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '(':
+			stack = append(stack, ')')
+		case '[':
+			stack = append(stack, ']')
+		case '{':
+			stack = append(stack, '}')
+		default:
+			if len(stack) == 0 || stack[len(stack)-1] != s[i] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
