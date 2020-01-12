5288d021caa27c94939fa9a7746f7ca9cca3e4cc
Tom Holmes <tom@wandb.com>
2020-01-11 16:15:51 -0800

    0323

--- 03/23-number-of-connected-components-in-an-undirected-graph/main.go
+++ 03/23-number-of-connected-components-in-an-undirected-graph/main.go
@@ -1,6 +1,16 @@
-package util
+package main
 
-// This is meant to be copied into a solution
+func main() {
+
+}
+
+func countComponents(n int, edges [][]int) int {
+	ds := newDisjointSubset(n)
+	for _, e := range edges {
+		ds.join(e[0], e[1])
+	}
+	return ds.countRoots()
+}
 
 type disjointSubset []int
 
@@ -24,28 +34,6 @@ func (d disjointSubset) parent(n int) int {
 func (d disjointSubset) join(a int, b int) {
 	pa, pb := d.parent(a), d.parent(b)
 	d[pa] = pb
-}
-
-// optional util methods
-
-func (d disjointSubset) isSingle() bool {
-	for i := range d {
-		if d.parent(i) != d.parent(0) {
-			return false
-		}
-	}
-	return true
-}
-
-func (d disjointSubset) getSiblings(n int) []int {
-	var res []int
-	p := d.parent(n)
-	for i := range d {
-		if d.parent(i) == p {
-			res = append(res, i)
-		}
-	}
-	return res
 }
 
 func (d disjointSubset) countRoots() int {
