a229792c54fd6d66b2b40776ab5e1a7d32ad2f7e
Tom Holmes <tom@wandb.com>
2020-01-22 21:03:24 -0800

    0416

---
+++ 04/16-partition-equal-subset-sum/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(canPartition([]int{1, 5, 11, 5}))
+	fmt.Println(canPartition([]int{1, 2, 3, 5}))
+
+	nums := make([]int, 200)
+	for i := range nums {
+		nums[i] = 2
+	}
+	nums[0] = 3
+	fmt.Println(canPartition(nums))
+
+	fmt.Println(canPartition([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 100}))
+	fmt.Println(canPartition([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 100}))
+}
+
+func canPartition(nums []int) bool {
+	total := 0
+	for _, n := range nums {
+		total += n
+	}
+
+	if total%2 == 1 {
+		return false
+	}
+
+	sums := make([]bool, total/2+1)
+	sums[0] = true
+	for _, n := range nums {
+		for i := len(sums) - 1; i >= 0; i-- {
+			if i+n > total/2 {
+				continue
+			}
+			sums[i+n] = sums[i+n] || sums[i]
+		}
+		if sums[total/2] {
+			return true
+		}
+	}
+	return false
+}
