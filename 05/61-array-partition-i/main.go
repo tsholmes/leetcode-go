36d0e71cf55b18915e7c1a21b89cb4fe17306507
Tom Holmes <tom@wandb.com>
2020-03-13 15:23:15 -0700

    0561

---
+++ 05/61-array-partition-i/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "sort"
+
+func main() {
+
+}
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	res := 0
+	for i := 0; i < len(nums); i += 2 {
+		res += nums[i]
+	}
+	return res
+}
