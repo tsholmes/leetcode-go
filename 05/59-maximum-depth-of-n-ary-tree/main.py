9a54cdf900270c112f3d676a79fd2210b4bc4816
Tom Holmes <tom@wandb.com>
2020-03-13 15:18:36 -0700

    0559

---
+++ 05/59-maximum-depth-of-n-ary-tree/main.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        res = 0
+        for c in root.children:
+            res = max(res, self.maxDepth(c))
+
+        return res + 1
